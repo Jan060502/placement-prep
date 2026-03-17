6de805c7e682101a277a957ec5bfc92357d1bafd
Jan060502 <113925493+Jan060502@users.noreply.github.com>
2026-03-17 23:02:22 +0530

    Add files via upload
    arraylist create

---
+++ ArrayListCreate.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class ArrayListCreate {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+
+        for(int num : list) {
+            System.out.println(num);
+        }
+    }
+}
